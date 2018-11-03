fe75270905e1fb4c434332b66f3cfc740f7f6f46
sysublackbear <2295869255@qq.com>
2018-11-04 02:37:05 +0800

    Update deque.impl.h

--- src/deque.impl.h
+++ src/deque.impl.h
@@ -138,6 +138,7 @@ namespace TinySTL{
 		}
 	}//end of Detail namespace
 
+	// 判断是否到deque末尾
 	template<class T, class Alloc>
 	bool deque<T, Alloc>::back_full()const{
 		return map_[mapSize_ - 1] && (map_[mapSize_]) == end().cur_;
@@ -158,18 +159,20 @@ namespace TinySTL{
 	template<class Iterator>
 	void deque<T, Alloc>::deque_aux(Iterator first, Iterator last, std::false_type){
 		difference_type mid = (last - first) / 2;
+		// 尽量往两个方向扩散，保证尽可能不倾斜
 		for (auto it = first + mid; it != first - 1; --it)
 			(*this).push_front(*it);
 		for (auto it = first + mid + 1; it != last; ++it)
 			(*this).push_back(*it);
 	}
+	// 空deque初始化
 	template<class T, class Alloc>
 	void deque<T, Alloc>::init(){
-		mapSize_ = 2;
-		map_ = getANewMap(mapSize_);
-		beg_.container_ = end_.container_ = this;
+		mapSize_ = 2;  // 默认2个map
+		map_ = getANewMap(mapSize_);  // 构造map
+		beg_.container_ = end_.container_ = this;  // 最开始没有元素,所以指向同一块空间
 		beg_.mapIndex_ = end_.mapIndex_ = mapSize_ - 1;
-		beg_.cur_ = end_.cur_ = map_[mapSize_ - 1];
+		beg_.cur_ = end_.cur_ = map_[mapSize_ - 1];  // 一开始位于最后一个存储区的空间
 	}
 	template<class T, class Alloc>
 	T *deque<T, Alloc>::getANewBuck(){
@@ -179,7 +182,7 @@ namespace TinySTL{
 	T** deque<T, Alloc>::getANewMap(const size_t size){
 		T **map = new T*[size];
 		for (int i = 0; i != size; ++i)
-			map[i] = getANewBuck();
+			map[i] = getANewBuck();  // 每个map需要分配一段连续空间
 		return map;
 	}
 	template<class T, class Alloc>
@@ -192,6 +195,8 @@ namespace TinySTL{
 		// 强制规定每个存储区的大小固定为64字节
 		return (size_t)EBucksSize::BUCKSIZE;
 	}
+	// clear()用于清除整个deque，请注意，deque的最初状态（无任何元素时）
+	// 保有一个缓冲区，因此，clear()完成之后恢复初始状态，也一样要保留一个缓冲区
 	template<class T, class Alloc>
 	void deque<T, Alloc>::clear(){
 		/*for (int i = 0; i != mapSize_; ++i)
@@ -252,6 +257,7 @@ namespace TinySTL{
 	template<class T, class Alloc>
 	deque<T, Alloc>::deque()
 		:mapSize_(0), map_(0){}
+	// value_type()：默认值
 	template<class T, class Alloc>
 	deque<T, Alloc>::deque(size_type n, const value_type& val = value_type()){
 		deque();
@@ -263,6 +269,7 @@ namespace TinySTL{
 		deque();
 		deque_aux(first, last, typename std::is_integral<InputIterator>::type());
 	}
+	// 深拷贝
 	template<class T, class Alloc>
 	deque<T, Alloc>::deque(const deque& x){
 		mapSize_ = x.mapSize_;
@@ -278,30 +285,31 @@ namespace TinySTL{
 	}
 	template<class T, class Alloc>
 	void deque<T, Alloc>::reallocateAndCopy(){
-		auto newMapSize = getNewMapSize(mapSize_);
-		T** newMap = getANewMap(newMapSize);
+		auto newMapSize = getNewMapSize(mapSize_);  // 扩大mapSize_,直接翻大两倍
+		T** newMap = getANewMap(newMapSize);  // 重新分配map
 		size_t startIndex = newMapSize / 4;
 		for (int i = 0; i + beg_.mapIndex_ != mapSize_; ++i)
 			for (int j = 0; j != getBuckSize(); ++j)
 				newMap[startIndex + i][j] = map_[beg_.mapIndex_ + i][j];
 
-		size_t n = beg_.cur_ - map_[beg_.mapIndex_];
+		size_t n = beg_.cur_ - map_[beg_.mapIndex_];  // 原来的偏移量
 		auto size = this->size();
 		auto b = beg_, e = end_;
 		clear();
 		mapSize_ = newMapSize;
 		map_ = newMap;
-		beg_ = iterator(startIndex, newMap[startIndex] + n, this);
+		beg_ = iterator(startIndex, newMap[startIndex] + n, this);  // 新的偏移量
 		end_ = beg_ + size;
 
 	}
 	template<class T, class Alloc>
 	void deque<T, Alloc>::push_back(const value_type& val){
 		if (empty()){
+			// 队列为空,分配空间
 			init();
 		}
 		else if (back_full()){
-			reallocateAndCopy();
+			reallocateAndCopy();  // 重新分配空间，扩大为原来的两倍
 		}
 		//*end_ = val;
 		//bug fix
@@ -316,8 +324,8 @@ namespace TinySTL{
 		}
 		else if (front_full()){
 			reallocateAndCopy();
-		}
-		--beg_;
+		}  
+		--beg_;  // beg_和end_一开始就位于最后一个存储空间的第一位，这个值得注意!!!
 		//*beg_ = val;
 		//bug fix
 		//2015.01.02
@@ -325,13 +333,13 @@ namespace TinySTL{
 	}
 	template<class T, class Alloc>
 	void deque<T, Alloc>::pop_front(){
-		dataAllocator::destroy(beg_.cur_);
+		dataAllocator::destroy(beg_.cur_);  //析构当前空间
 		++beg_;
 	}
 	template<class T, class Alloc>
 	void deque<T, Alloc>::pop_back(){
 		--end_;
-		dataAllocator::destroy(end_.cur_);
+		dataAllocator::destroy(end_.cur_);  // 析构尾部
 	}
 	template<class T, class Alloc>
 	void deque<T, Alloc>::swap(deque<T, Alloc>& x){
